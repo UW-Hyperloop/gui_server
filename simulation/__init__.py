d35d24afb0d570be8a9697142dee559cb6248ab3
alexyu <alexyu2023@gmail.com>
2025-11-17 17:27:17 -0800

    only server client

--- simulation/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Simulation package for TBM GUI data simulation.
-"""
-
-from .data_simulator import DataSimulator
-
-__all__ = ['DataSimulator'] 
--- widgets/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Widgets package for TBM GUI components.
-"""
-
-from .linear_gauge import LinearGauge
-from .system_status import SystemStatusWidget
-from .terminal import TerminalWidget
-
-__all__ = ['LinearGauge', 'SystemStatusWidget', 'TerminalWidget'] 
